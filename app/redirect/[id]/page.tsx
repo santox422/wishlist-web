afeef6bd67fef0f229a5e0c64fc2c65c7a181ee6
Sebastian Seidel <sebastian.seidel422@gmail.com>
2026-01-17 13:54:46 +0100

    xyc

--- app/redirect/[id]/page.tsx
+++ app/redirect/[id]/page.tsx
@@ -239,7 +239,7 @@ export default function WishlistPreviewPage() {
 
         {/* Wishes Grid */}
         {wishes.length > 0 && (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
+          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
             {wishes.map((wish) => (
               <div
                 key={wish._id}
